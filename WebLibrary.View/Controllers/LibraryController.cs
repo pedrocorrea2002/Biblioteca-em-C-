63ae0b078e8b0031ecac462cd2ea22d1638db8a6
David Ramos <david.ramos@aedb.br>
2022-04-20 21:11:39 -0300

    Commit Inicial

---
+++ WebLibrary.View/Controllers/LibraryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebLibrary.View.Models;
+
+namespace WebLibrary.View.Controllers
+{
+    public class LibraryController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult livro() // Método "Livro"
+        {
+            return View();
+        }
+        public IActionResult Cliente() // Método "Cliente"
+        {
+            return View();
+        }
+        public IActionResult Emprestimo() // Método "Emprestimo"
+        {
+            return View();
+        }
+    }
+}
+
---
+++ WebLibrary.View/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebLibrary.View.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
